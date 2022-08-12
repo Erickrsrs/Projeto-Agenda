31b4b7618077a27cdfeb5cecf2aa0582c9499843
Erick <erick2003rocha@gmail.com>
2022-08-11 23:23:35 -0300

    chore: adding login view and correcting errors in springBoot header includes

--- routes.js
+++ routes.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const route = express.Router();
 const homeController = require("./src/controllers/homeController");
+const loginController = require("./src/controllers/loginController");
 
-route.get("/", homeController.paginaInicialGet);
-route.post("/", homeController.paginaInicialPost);
+//rotas da home
+route.get("/", homeController.index);
+
+//rotas de login
+route.get("/login", loginController.index);
 
 module.exports = route;
--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,12 +1,3 @@
-exports.paginaInicialGet = function (req, res) {
-  res.render("index", {
-    title: "Página Inicial",
-    numeros: [1, 2, 3, 4, 5],
-  });
-  return;
-};
-
-exports.paginaInicialPost = (req, res) => {
-  res.send(req.body);
-  return;
+exports.index = function (req, res) {
+  res.render("index");
 };
